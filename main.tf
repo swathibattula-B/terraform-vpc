095f2b31e9966d9a160a593b61112fdea112718d
swathi <battulaswathi15407@gmail.com>
2026-07-08 10:57:41 +0530

    terraform

--- main.tf
+++ main.tf
@@ -157,18 +157,18 @@ resource "aws_route" "database" {
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet)
-  subnet_id      = aws_public_subnet[count.index].id
+  subnet_id      = aws_subnet_public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet)
-  subnet_id      = aws_private_subnet[count.index].id
+  subnet_id      = aws_subnet_private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
   count = length(var.database_subnet)
-  subnet_id      = aws_database_subnet[count.index].id
+  subnet_id      = aws_subnet_database[count.index].id
   route_table_id = aws_route_table.database.id
 }
