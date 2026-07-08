08b4f672b621ffb99b6291a7f23d6db8c0811e78
swathi <battulaswathi15407@gmail.com>
2026-07-08 13:30:08 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
 output "az" {
     value = local.azs
 }
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_subnet" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet" {
+    value = aws_subnet.database[*].id
+}
