2b8f9434352b342df68cacfe496f3d0b4a2547e6
swathi <battulaswathi15407@gmail.com>
2026-07-08 10:21:17 +0530

    terraform

--- main.tf
+++ main.tf
@@ -125,7 +125,6 @@ resource "aws_eip" "nat" {
   )
 }
 
-
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
@@ -141,13 +140,6 @@ resource "aws_nat_gateway" "main" {
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
-}
-
-
-resource "aws_route" "private" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id  = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "private" {
